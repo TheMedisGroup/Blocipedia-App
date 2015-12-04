bfa2bf0146d450335309895b06b122d82f38d9aa
Michael Medis <TheMedisGroup@gmail.com>
2015-12-04 15:48:52 -0500

    Testing Email Confirmation

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "medis.michael@gmail.com"
+  layout 'mailer'
+end
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   address:           'smtp.mailgun.org',
   user_name:         ENV['MAILGUN_SMTP_LOGIN'],
   password:          ENV['MAILGUN_SMTP_PASSWORD'],
-  domain:            'appurlhere.com',
+  domain:            'desolate-coast-3425.herokuapp.com',
   authentication:    :plain,
   content_type:      'text/html'
 }
