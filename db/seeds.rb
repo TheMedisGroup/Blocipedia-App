b25d4c6919923f2b59b2e715e9222726fa8c3d82
Michael Medis <TheMedisGroup@gmail.com>
2015-12-06 22:40:01 -0500

    Devise Mailer updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+## Create Users
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save
+end
+
+## Create admin user for testing
+mike = User.new(
+  name: 'Mike Medis',
+  email: 'medis.michael@gmail.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+mike.skip_confirmation!
+mike.save
+
+users = User.all
+puts "#{User.count} users created."
+
+## Create wikis
+20.times do
+  wiki = Wiki.create!(
+    user: users.sample,
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+wikis = Wiki.all
+puts "#{wikis.count} wikis created."
